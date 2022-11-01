eeee2b0d688e5da4851d4a4b4b10d81060c31899
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-01 19:38:24 +0300

    Average calculate

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        int value = scanner.nextInt();
+        int sum=value;
+        int count = 1;
+        while (true) {
+            value = scanner.nextInt();
+            if(value==0) {
+                break;
+            } else {
+                sum += value;
+                count++;
+            }
+        }
+        System.out.println(sum / count);
     }
-
 }
